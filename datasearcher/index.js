0e0488a8c5d49f631ec08a271b2b961b96253255
Genius1512 <79528697+Genius1512@users.noreply.github.com>
2022-01-30 17:22:29 +0100

    next try

--- datasearcher/index.js
+++ datasearcher/index.js
@@ -1,23 +1,18 @@
-function getJSON(url) {
+function fetchSite(url) {
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", url)
-    xhr.send()
+    xhr.open("GET", url);
+    xhr.send();
 
     xhr.onload = function() {
-        if (xhr.status != 200) { // analyze HTTP status of the response
-            alert(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
-        } else { // show the result
-            alert(`Done, got ${xhr.response.length} bytes`); // response is the server response
-            return JSON.parse(xhr.response)
+        if (xhr.status != 200) {
+            console.error("Error " + xhr.status + ": " + xhr.statusText);
+        } else {
+            return xhr.response;
         }
-    };
+    }
 }
 
-
 function search() {
-    var searchTerm = document.getElementById("search-term-input").value;
-    var profilePre = document.getElementById("profile-text");
-    var url = "/athene/files/data.json";
-
-    console.log(getJSON("/athene/files/data.json").students["silvan.schmidt"]["id"]);
+    var searchTerm = document.getElementById("search-term-input");
+    console.log(fetchSite(searchTerm));
 }
