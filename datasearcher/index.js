4e3d56c9a52f9156890f5c08199702ac908f7412
Genius1512 <79528697+Genius1512@users.noreply.github.com>
2022-01-30 13:01:01 +0100

    added getjson function

--- datasearcher/index.js
+++ datasearcher/index.js
@@ -1,6 +1,17 @@
+function getJSON(url) {
+    fetch('https://yesno.wtf/api')  
+        .then(res => res.json())  
+        .then((out) => {  
+            return out;
+        })  
+        .catch(err => {  
+            throw err  
+    });
+}
+
 function search() {
     var searchTerm = document.getElementById("search-term-input").value;
     var profilePre = document.getElementById("profile-text");
 
-    
+    profilePre.innerHTML(getJSON("https://genius1512.github.io/athene/files/data.json"))
 }
