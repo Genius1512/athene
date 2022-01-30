3902b6fa73c81baf07a8e99b72da804c2a255277
Genius1512 <79528697+Genius1512@users.noreply.github.com>
2022-01-30 13:37:53 +0100

    hotfix

--- datasearcher/index.js
+++ datasearcher/index.js
@@ -1,17 +1,21 @@
 function getJSON(url) {
-    fetch('https://yesno.wtf/api')  
-        .then(res => res.json())  
-        .then((out) => {  
-            return out;
-        })  
-        .catch(err => {  
-            throw err  
-    });
+    var out = "";
+    fetch(url)
+        .then(function(resp) {
+            return resp.json()
+        })
+        .then(function(data) {
+            out = data;
+        })
+
+    return out
 }
+
 
 function search() {
     var searchTerm = document.getElementById("search-term-input").value;
     var profilePre = document.getElementById("profile-text");
+    var url = "http://genius1512.github.io/athene/files/data.json";
 
-    profilePre.innerHTML(getJSON("https://genius1512.github.io/athene/files/data.json"))
+    console.log(getJSON(url));
 }
