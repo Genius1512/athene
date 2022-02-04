2d4cd1c7479c3a25fafd3fd7a802daa6d271d364
Genius1512 <silvan.genius@gmail.com>
2022-02-04 14:05:47 +0100

    done

--- datasearcher/index.js
+++ datasearcher/index.js
@@ -1,24 +1,60 @@
-const profileTemplate = document.getElementById("profile-template");
-const profilesContainer = document.getElementById("profiles-container");
-const searchInput = document.getElementById("search-input");
+const userCardTemplate = document.querySelector("[data-user-template]")
+const userCardContainer = document.querySelector("[data-user-cards-container]")
+const searchInput = document.querySelector("[data-search]")
 
-let profiles;
+let profiles = []
 
-// fetch json data
-async function getProfiles() {
-    let response = await fetch("https://genius15125.github.io/athene/files/data.json");
+searchInput.addEventListener("input", e => {
+    const value = e.target.value.toLowerCase()
+    profiles.forEach(profile => {
+        const isVisible =
+            profile.name.toLowerCase().includes(value) ||
+            profile.id.toLowerCase().includes(value) ||
+            profile.class.toLowerCase().includes(value) ||
+            profile.hash.toLowerCase().includes(value)
+        profile.element.classList.toggle("hide", !isVisible)
+    })
+})
 
-    var json;
-    if (response.ok) {
-        json = response.json();
-    } else {
-        console.error("HTTPError: " + response.status);
-    }
+fetch("https://genius1512.github.io/athene/files/data.json")
+    .then(res => res.json())
+    .then(data => ({
+        ...data.students,
+        ...data.teachers
+    }))
+    .then(function(data) {
+        newData = [];
 
-    return json;
-}
-
-async function getData() {
-    profiles = await getProfiles();
-}
-getData();
+        for (const profile in data) {
+            newData.unshift(data[profile])
+            try {
+                if (newData[0].class == undefined) {
+                    newData[0].class = "None"
+                }
+                newData[0].identifier = profile
+            } catch (TypeError) {
+            }
+        }
+        
+        return newData;
+    })
+    .then(data => {
+        profiles = data.map(profile => {
+            const card = userCardTemplate.content.cloneNode(true).children[0]
+            const header = card.querySelector("[data-header]")
+            const body = card.querySelector("[data-body]")
+            header.textContent = profile.identifier
+            body.textContent = `Name: ${profile.name}
+ID: ${profile.id}
+Class: ${profile.class}
+Hash: ${profile.hash}`
+            userCardContainer.append(card)
+            return {
+                name: profile.name,
+                id: profile.id,
+                class: profile.class,
+                hash: profile.hash,
+                element: card
+            }
+        })
+    })
