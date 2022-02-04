08bc74573a976adfea160cabf5a2090973ea676c
Genius1512 <silvan.genius@gmail.com>
2022-02-04 15:38:21 +0100

    htofix

--- datasearcher/index.js
+++ datasearcher/index.js
@@ -11,7 +11,8 @@ searchInput.addEventListener("input", e => {
             profile.name.toLowerCase().includes(value) ||
             profile.id.toLowerCase().includes(value) ||
             profile.class.toLowerCase().includes(value) ||
-            profile.hash.toLowerCase().includes(value)
+            profile.hash.toLowerCase().includes(value) ||
+            profile.rawName.toLowerCase().includes(value)
         profile.element.classList.toggle("hide", !isVisible)
     })
 })
@@ -26,13 +27,11 @@ fetch("https://genius1512.github.io/athene/files/data.json")
         newData = [];
 
         for (const profile in data) {
-            newData.unshift(data[profile])
-            try {
-                if (newData[0].class == undefined) {
-                    newData[0].class = "None"
-                }
-            } catch (TypeError) {
+            data[profile]["rawName"] = profile
+            if (data[profile].class == undefined) {
+                data[profile].class = "None"
             }
+            newData.unshift(data[profile])
         }
         
         return newData;
@@ -42,7 +41,7 @@ fetch("https://genius1512.github.io/athene/files/data.json")
             const card = userCardTemplate.content.cloneNode(true).children[0]
             const header = card.querySelector("[data-header]")
             const body = card.querySelector("[data-body]")
-            header.textContent = profile
+            header.textContent = profile.rawName
             body.textContent = `Name: ${profile.name}
 ID: ${profile.id}
 Class: ${profile.class}
@@ -53,6 +52,7 @@ Hash: ${profile.hash}`
                 id: profile.id,
                 class: profile.class,
                 hash: profile.hash,
+                rawName: profile.rawName,
                 element: card
             }
         })
